21cc75a800d365e58c824191bd40fc8479fb6d84
DanInamori <75373095+DanInamori@users.noreply.github.com>
2021-01-23 14:22:35 +0900

    ルーム機能の実装3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
-  resources :rooms, only: [:new, :create, :show, :destroy] do
+  resources :rooms, only: [:new, :create, :show, :destroy, :edit, :update] do
     resources :shift_creators, only: [:new, :create]
-    resources :schedules, only: [:new, :create, :show] do
+    resources :schedules, only: [:new, :create, :show, :edit, :update] do
       resources :shifts, only: [:new, :create, :show]
     end
     member do
